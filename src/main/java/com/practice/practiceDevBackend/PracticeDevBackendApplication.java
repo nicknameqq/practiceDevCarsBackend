e5a6fc3b278c038c4c5561cd2a185ac37f17fd2e
nicknameqq <112827109+nicknameqq@users.noreply.github.com>
2026-08-22 02:39:47 +0300

    Initial Spring Boot backend setup

---
+++ src/main/java/com/practice/practiceDevBackend/PracticeDevBackendApplication.java
@@ -0,0 +1,13 @@
+package com.practice.practiceDevBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeDevBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeDevBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/practiceDevBackend/PracticeDevBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.practiceDevBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeDevBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
